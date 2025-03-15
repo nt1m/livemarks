5f5ca29d0b5ef7206ef0a5bc94b0e20c5b94b9c7
BynariStar <BynariStar@users.noreply.github.com>
2025-03-15 19:18:57 +0200

    fix: Tab Stash naming

--- background/background.js
+++ background/background.js
@@ -253,7 +253,7 @@ const LivemarkUpdater = {
       return;
     }
 
-    // Tab Stasher extension compatibility
+    // Tab Stash extension compatibility
     if (folder.title === "Tab Stash" && folder.parentId === "unfiled_____") {
       return;
     }
